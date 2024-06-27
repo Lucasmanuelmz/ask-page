ad51d111228f1b15fa477dffe6b12e10ab5e0bc6
Lucas Manueel <testadormoz@gmail.com>
2024-06-27 19:55:18 +0200

    Configurei o banco de dados com sequelize

--- database/database.js
+++ database/database.js
@@ -1,18 +1,16 @@
-const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize(
-    'Questions',
-    'root',
-    '4026.Test@Lucas#Manuel',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-)
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("Questions", "root", "4026.Test@Lucas#Manuel", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
-sequelize.authenticate().then(() => {
-    return 'Conectado com sucesso!'
-}).catch((err) => {
-   return err;
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    return "Conectado com sucesso!";
+  })
+  .catch((err) => {
+    return err;
+  });
 
 module.exports = sequelize;
