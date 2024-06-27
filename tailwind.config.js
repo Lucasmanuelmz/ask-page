5d43b7c1fe786ddc584f2c8ce85baada561452f4
Lucas Manueel <testadormoz@gmail.com>
2024-06-27 20:04:36 +0200

    Arquivo de configurações do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './views/**/*.ejs',
-    './public/**/*.css',
-  ],
+  content: ["./views/**/*.ejs", "./public/**/*.css"],
   theme: {
     extend: {},
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }
   },
-  plugins: [],
-}
+  plugins: [
+  ]
+};
 
