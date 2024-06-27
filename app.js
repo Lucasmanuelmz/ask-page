4b275f33ab4aeb8b75394bf054400a9cd3d5d376
Lucas Manueel <testadormoz@gmail.com>
2024-06-27 19:53:03 +0200

    criei uma nova rota e atualizei uma para post

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Questions = require('./database/questionModel');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Questions = require("./database/questionModel");
 
 const port = 3000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-
-app.post('/post-question', (req, res) => {
-    let {question, description} = req.body;
-
-    Questions.create({
-        question: question,
-        description: description
-    }).then(() => {
-        res.rederect('/home')
-    })
-
-})
-
-app.get('/home', (req, res) => {
-    Questions.findAll({raw: true}).then((question) => {
-        res.render('home.ejs', {
-            question: question
-        })
-    })
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/give-answer/:id', (req, res) => {
-    let id = req.params.id;
-    res.render('answer.ejs', {
-        id: id
-    })
-})
+app.post("/post-question", (req, res) => {
+  let { question, description } = req.body;
 
-app.listen(port, (error)=> {
-     function verifyConnection() {
-        try{
-            if(!error) {
-              return 'You connected successfull!'
-            } else {
-                return 'Connection danied!'
-            }
-        }catch (err) {
-        if(err){
-            console.error('Erro: '+err)
-        }
-        } 
+  Questions.create({
+    question: question,
+    description: description,
+  }).then(() => {
+    res.redirect("/home");
+  });
+});
+
+app.get("/home", (req, res) => {
+  Questions.findAll({ raw: true }).then((questions) => {
+    res.render("home.ejs", {
+      questions: questions,
+    });
+  });
+});
+
+app.get("/give-answer/:id", (req, res) => {
+  let id = req.params.id;
+  res.render("answer.ejs", {
+    id: id,
+  });
+});
+
+app.listen(port, (error) => {
+  function verifyConnection() {
+    try {
+      if (!error) {
+        return "You connected successfull!";
+      } else {
+        return "Connection danied!";
+      }
+    } catch (err) {
+      if (err) {
+        console.error("Erro: " + err);
+      }
     }
-    verifyConnection()
-})
+  }
+  verifyConnection();
+});
